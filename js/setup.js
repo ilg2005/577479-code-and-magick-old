fe81bb8c728efa18765a6163523804127f813f05
ilg2005 <igoldfarb10@gmail.com>
2018-06-06 16:55:21 +0300

    объявлена функция генерации случайных данных

---
+++ js/setup.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var WIZARDS_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARDS_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARDS_COATS_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARDS_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomFeature = function (array) {
+  return array[Math.round(Math.random() * (array.length - 1))];
+};
+
+console.log(getRandomFeature(WIZARDS_EYES_COLORS));
