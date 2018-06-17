685dec247345871cc8e890be420c5164f74a95eb
ilg2005 <igoldfarb10@gmail.com>
2018-06-17 14:34:39 +0300

    добавлен массив WIZARDS_FIREBALL_COLORS

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,7 @@ var WIZARDS_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мари�
 var WIZARDS_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARDS_COATS_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARDS_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
