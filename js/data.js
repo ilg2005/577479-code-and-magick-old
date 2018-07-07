950f046296f3177968b700e75c839067143b4b8e
ilg2005 <igoldfarb10@gmail.com>
2018-07-07 15:04:31 +0300

    имена волшебников вынесены за пределы глобального объекта

--- js/data.js
+++ js/data.js
@@ -1,15 +1,15 @@
 'use strict';
 
 (function () {
+  var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   window.data = {
-    FIRST_NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
-    LAST_NAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
     COATS_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
     EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
     FIREBALL_COLORS: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'],
     createWizard: function () {
       return {
-        name: window.util.getRandomFeature(window.data.FIRST_NAMES) + ' ' + window.util.getRandomFeature(window.data.LAST_NAMES),
+        name: window.util.getRandomFeature(FIRST_NAMES) + ' ' + window.util.getRandomFeature(LAST_NAMES),
         coatColor: window.util.getRandomFeature(window.data.COATS_COLORS),
         eyesColor: window.util.getRandomFeature(window.data.EYES_COLORS)
       };
