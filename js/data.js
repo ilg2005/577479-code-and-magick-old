25f5701efb954e12e5ef3d4e1beac09050c8044c
ilg2005 <igoldfarb10@gmail.com>
2018-07-17 20:52:55 +0300

    Реализована загрузка данных

--- js/data.js
+++ js/data.js
@@ -3,6 +3,18 @@
 (function () {
   var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
   var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
+  var onSuccessLoad = function (response) {
+    alert('Данные загружены успешно!');
+    console.log(response);
+  };
+
+  var onErrorLoad = function (message) {
+    alert(message);
+  };
+
+  window.backend.load(onSuccessLoad, onErrorLoad);
+
   window.data = {
     COATS_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
     EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
@@ -15,4 +27,5 @@
       };
     }
   };
+
 })();
